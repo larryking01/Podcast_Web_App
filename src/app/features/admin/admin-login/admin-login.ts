e30946c2ac72b27f8169a2132cbccd894b425da4
Faisal <pros3sa@gmail.com>
2025-07-09 12:10:38 +0000

    fix: add routes to the main.ts. Pages were not being reders

--- src/app/features/admin/admin-login/admin-login.ts
+++ src/app/features/admin/admin-login/admin-login.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Navbar } from '../../../shared/components/navbar/navbar';
 import { Footer } from '../../../shared/components/footer/footer';
 
+
 @Component({
   selector: 'app-admin-login',
   imports: [Navbar, Footer],
