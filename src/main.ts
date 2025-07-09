c847469d7e9492a17e60fd108e7bffa8137c48a7
larry <larryking8118@gmail.com>
2025-07-09 12:34:18 +0000

    Feature: Integrate backend services and fix routing bootstrap bug

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/app';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { TokenInterceptor } from './app/auth/token-interceptor';
 
+
+
 bootstrapApplication(App, {
   providers: [
+    provideRouter(routes),
     provideHttpClient(withInterceptorsFromDi()),
     {
       provide: HTTP_INTERCEPTORS,
