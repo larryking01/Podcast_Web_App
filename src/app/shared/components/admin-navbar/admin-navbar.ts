7627b039d9b6019296829009e7eefdd92edf7a6c
Faisal <pros3sa@gmail.com>
2025-07-10 05:32:48 +0000

    feat: add router lonk for admin navbar

--- src/app/shared/components/admin-navbar/admin-navbar.ts
+++ src/app/shared/components/admin-navbar/admin-navbar.ts
@@ -1,8 +1,9 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-admin-navbar',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './admin-navbar.html',
   styleUrl: './admin-navbar.scss'
 })
