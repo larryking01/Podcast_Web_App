895a64f76a832ecefbf8a90e44942f8d717156ed
larry <larryking8118@gmail.com>
2025-07-11 13:00:12 +0000

    Feature: Implement episode details

--- src/app/features/public/episode-details/episode-details.ts
+++ src/app/features/public/episode-details/episode-details.ts
@@ -2,6 +2,11 @@ import { Component, inject, OnInit } from '@angular/core';
 import { Footer } from '../../../shared/components/footer/footer';
 import { Navbar } from '../../../shared/components/navbar/navbar';
 import { EpisodesService } from '../../../core/services/episodes.service';
+import { Episode } from '../../../core/models/episodes.interface';
+import { EpisodeResponse } from '../../../core/models/episodes.interface';
+import { ActivatedRoute } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-episode-details',
@@ -12,10 +17,42 @@ import { EpisodesService } from '../../../core/services/episodes.service';
 export class EpisodeDetails implements OnInit {
 
   episodesService = inject( EpisodesService )
+  activatedRoute = inject( ActivatedRoute )
+  selectedEpisodeID: string | null = null
+  selectedEpisode: Episode | undefined = undefined
+
+
+  episodes$: EpisodeResponse = {
+    status: 'idle',
+    data: [],
+    meta: {
+      total: 1,
+      page: 1,
+      last_page: 1
+    }
+  }
+
+
+  episodesArray: Episode[] = []
 
 
   ngOnInit(): void {
-    
+    this.selectedEpisodeID = this.activatedRoute.snapshot.paramMap.get('id')
+    if( this.selectedEpisodeID ) {
+      this.episodesService.getEpisodeByID( Number.parseInt(this.selectedEpisodeID) )
+      .subscribe({
+        next: (( selectedEpisode ) => {
+          this.selectedEpisode = selectedEpisode
+          console.log('selected episode = ', this.selectedEpisode )
+        }),
+        error: (( error ) => console.log('error fetching episode details = ', error))
+      })
+    }
+    else {
+      // display error banner here.
+      alert('Failed to fetch episode details')
+    }
+
   }
 
 
