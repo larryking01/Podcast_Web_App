08b694d3e3f44ce6b9ca6c99cc650d5ba5dc3f2a
larry <larryking8118@gmail.com>
2025-07-12 04:28:07 +0000

    Featture: Refactor public side ui to be more appealing

--- src/app/core/services/playlists.ts
+++ src/app/core/services/playlists.ts
@@ -1,9 +1,31 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject, OnInit } from '@angular/core';
+import { PlaylistsService } from './playlists.service';
+import { Playlist } from '../models/playlists.interface';
+
 
 @Injectable({
   providedIn: 'root'
 })
-export class Playlists {
+export class Playlists implements OnInit {
 
-  constructor() { }
+  playListService = inject( PlaylistsService )
+  playListsArray: Playlist[] = []
+
+
+  ngOnInit(): void {
+    
+  }
+
+
+  // getAllPlaylists() {
+  //   this.playListService.getAllPlaylists().subscribe({
+  //     next: ( playListsResponse ) => {
+  //       console.log("playlists response = ", playListsResponse )
+  //       this.playListsArray = playListsResponse.data
+
+  //     }
+  //   })
+  // }
+
+
 }
